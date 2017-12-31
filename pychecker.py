bc6b7a6556e812117a43210c1aaea9d2e31b137c
Hacker4ma <hackerama@protonmail.com>
2017-12-31 16:18:17 -0200

    delete pychecker

--- pychecker.py
+++
@@ -1,8 +0,0 @@
-#/usr/bin/python
-
-import requests
-
-payload  = { 'username' : 'egregora.team@gmail.com', 'password' : '@nolimits42'}
-req = requests.post('https://adm.force.com/parceirosdirecionalengenharia/login', data=payload).text 
-
-print req
